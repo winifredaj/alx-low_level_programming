196f964a3453df25977ba542e81fdc5ab5a7d880
winifredaj <winifredajiniran@gmail.com>
2023-08-12 10:56:53 -0700

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
-#include <studio.h>
-int main(){
-
-	puts("Programming is like building a multilingual puzzle /n");
+#include <stdio.h>
+/**
+ * main - Prints quote
+ *
+ * Return: zero on success
+ *
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
         return(0);
 
 }
