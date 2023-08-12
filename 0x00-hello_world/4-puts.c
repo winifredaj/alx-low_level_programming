cf00fb99baeb52a7b659cbf3b0ddfd2e359de15a
winifredaj <winifredajiniran@gmail.com>
2023-08-12 10:50:40 -0700

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,7 @@
-#!/bin/bash
-gcc -E int putchar("Programming is like building a multilingual puzzle /n"); return(0);
+#include <studio.h>
+int main(){
+
+	puts("Programming is like building a multilingual puzzle /n");
+        return(0);
+
+}
