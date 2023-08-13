7b6c6f168a79008e877327543d39f87be4a7effd
winifredaj <winifredajiniran@gmail.com>
2023-08-13 16:26:15 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main -prints function
+ *
+ * Return: zero on success
+ *
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
