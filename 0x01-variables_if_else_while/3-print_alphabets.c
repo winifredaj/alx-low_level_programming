f89a1c25b16f03cdf43d61edef2dd06f04699b80
winifredaj <winifredajiniran@gmail.com>
2023-08-13 15:34:14 -0700

    alphaBETS

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main -prints function
+ *
+ * Return: zero on success
+ *
+ */
+int main(void)
+{
+	char alphabet;
+	char Alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	for (Alphabet ='A'; Alphabet <= 'Z'; Alphabet++)
+	{
+		putchar(Alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
