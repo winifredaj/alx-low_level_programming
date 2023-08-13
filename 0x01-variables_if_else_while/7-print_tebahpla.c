7441b9d4711ce8ca0db9ffbe0428dc137ad7c15f
winifredaj <winifredajiniran@gmail.com>
2023-08-13 16:47:28 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main -prints alphabet in reverse
+ *
+ * Return: zero on success
+ *
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter <= 'z' letter--)
+	{
+		putchar(letter);
+		letter--;
+	}
+	putchar('\n');
+	return (0);
+}
