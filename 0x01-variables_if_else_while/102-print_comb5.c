64ee5ee26bf9feb27a7f6e985ba2b69eff31db47
winifredaj <winifredajiniran@gmail.com>
2023-08-14 15:50:26 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,14 +13,14 @@ int main(void)
 
 	for (x = 0; x <= 99; x++)
 	{
-		for (y = 0; y <= 99; y++)
+		for (y = x + 1; y <= 99; y++)
 		{
 			putchar((x / 10) + '0');
 			putchar((x % 10) + '0');
 			putchar(' ');
 			putchar((y / 10) + '0');
 			putchar((y % 10) + '0');
-			if ((x < 98 ) | (y < 99))
+			if ((x < 98) | (y < 99))
 			{
 				putchar(',');
 				putchar(' ');
