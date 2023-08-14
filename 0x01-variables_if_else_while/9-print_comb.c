26fa4e305f634bbbe02bd091abc7196f22244d20
winifredaj <winifredajiniran@gmail.com>
2023-08-13 17:55:50 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,18 +10,18 @@
 int main(void)
 {
 	int num;
+	
 	num = 48;
-
 	while (num <= 57)
 	{
-	putchar(num);
+		putchar(num);
 		if (num < 57) 
 		{
 			putchar(',');
 			putchar(' ');
 		}
 		num++;
-	}
+	
 	putchar('\n');
 	return (0);
 }
