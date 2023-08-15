14fcd9575566300f40da8f9ed9750cfdde98221f
winifredaj <winifredajiniran@gmail.com>
2023-08-15 08:04:22 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main -prints function
+ *
+ * Return: zero on success
+ *
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
