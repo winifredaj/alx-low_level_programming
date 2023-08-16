ce7237e7a535c85cd1b29d0f22e15ac3ca949688
winifredaj <winifredajiniran@gmail.com>
2023-08-16 14:13:08 -0700

    There is no such thing as absolute value in this world. You can only estimate what a thing is worth to you

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -5,9 +5,16 @@
  *
  * @n: character to be change to absolute integr
  *
- *Return: integer if absolute
+ *Return: n if integer absolute, otherwise -n
  */
 int _abs(int n)
 {
-	putchar('n');
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
 }
