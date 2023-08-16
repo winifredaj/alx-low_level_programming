3dead9da121f34c641bcf88be00e85547a65456b
winifredaj <winifredajiniran@gmail.com>
2023-08-16 12:00:27 -0700

    isLower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - checks code
+ *
+ * Return: always zero
+ */
+int main (void)
+{
+	char lowerCase;
+
+	for (lowerCase = 'a'; lowerCase = 'z'; lowerCase++)
+	{
+		putchar(lowerCase);
+	}
+	putchar('\n');
+}
+/**
+ * int_islower(int c) - checks for lowercase
+ *
+ *
+int _islower(int c)
+{
+	if ((lowerCase >= 97) && (lowerCase <= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
