50e3e1fce2c905dfb4750a8a986506419af0ad80
winifredaj <winifredajiniran@gmail.com>
2023-08-18 02:46:47 -0700

    I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,11 +11,11 @@ void print_most_numbers(void)
 
 	for (i = 48; i <= 57; i++)
 	{
-		if (i == 50 | i == 52)
+		if (i == 50 || i == 52)
 		{
 			i++;
 		}
 		_putchar(i);
 	}
-	_putchar(i);
+	_putchar('\n');
 }
