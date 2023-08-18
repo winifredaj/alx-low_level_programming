5998d0a6443088d275cd8a01b3145eebbd9ca844
winifredaj <winifredajiniran@gmail.com>
2023-08-18 04:39:31 -0700

    The shortest distance between two points is a straight line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,18 +1,22 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
+ * print_diagonal - print diagonal line
+ * @i: integer
+ * @j: integer
  * Return: Always 0.
  */
 void print_diagonal(int n)
 {
-	while (n >= 1)
+	int i, j;
+	
+	for (i = 1; i > n; i++)
 	{
+		for (j = 1; j > i; j++)
+			_putchar(' ');
 		_putchar('\\');
-		_putchar(' ');
-		n++;
 		_putchar('\n');
 	}
-	_putchar('\n');
+	if (n < 1)
+		_putchar('\n');
 }
