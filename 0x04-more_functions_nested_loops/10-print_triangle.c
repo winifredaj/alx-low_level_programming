18bb788ee0973094bdbea45b3b65c44da5935982
winifredaj <winifredajiniran@gmail.com>
2023-08-19 14:15:56 -0700

    Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,20 +7,16 @@
  */
 void print_triangle(int size)
 {
-	int i, j; 
+	int i, j;
 
 	for (i = 1; i <= size; i++)
 	{
 		for (j = size - i; j > 0; j--)
-		{
 			_putchar(' ');
-		}
 		for (j = i; j > 0; j--)
-		{
 			_putchar('#');
-		}
 		_putchar('\n');
 	}
-	if (size < 1);
+	if (size < 1)
 		_putchar('\n');
 }
