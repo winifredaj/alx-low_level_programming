2f26fb629ba3157b2af2ccc456e2010ab22e2894
winifredaj <winifredajiniran@gmail.com>
2023-08-19 13:43:01 -0700

    Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * priny_triangle - print triangle using #
+ * @size: size of triangle tobe printed
+ * Return: Always 0.
+ */
+void print_triangle(int size)
+{
+	int i, j; 
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = size - 1; j > 0; j--)
+		{
+			_putchar(' ');
+		}
+		for (j = i; j > 0; j--)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	if (size < 1);
+		_putchar('\n');
+}
