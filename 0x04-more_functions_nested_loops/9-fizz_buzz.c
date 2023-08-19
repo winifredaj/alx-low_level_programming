06c8141b573cf18dfb51800593dc716c598c1fb7
winifredaj <winifredajiniran@gmail.com>
2023-08-19 12:33:34 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,17 +9,17 @@ int main(void)
 {
 	int x;
 
-	for (x = '1'; x <= '100'; x++)
+	for (x = 1; x <= 100; x++)
 	{
-		if ((x % '3') == '0')
+		if ((x % 3) == 0)
 		{
 			_putchar('Fizz');
 		}
-		else if ((x % '5') == '0')
+		else if ((x % 5) == 0)
 		{
 			_putchar('Buzz');
 		}
-		else if ((x % '3') == '0') && ((x % '5') == '0')
+		else if ((x % 3) == 0) && ((x % 5) == 0)
 		{
 			_putchar('FizzBuzz');
 		}
