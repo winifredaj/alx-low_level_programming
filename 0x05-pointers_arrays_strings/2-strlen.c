d9acb8c97c2094bd3d840a00ed82d474828002dd
winifredaj <winifredajiniran@gmail.com>
2023-08-22 15:36:22 -0700

    This report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,9 +2,10 @@
 #include <stdio.h>
 
 /**
- * strlen- print lenght of string 
- * @n: string 
- * Return: number of string
+ * _strlen- print length of string
+ * @s: string to count
+ * @n: string
+ * Return: string length
  */
 int _strlen(char *s)
 {
