f1507dfa96064a4ae78c3876e772b36f45973a49
winifredaj <winifredajiniran@gmail.com>
2023-08-22 15:40:53 -0700

    This report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,14 +4,12 @@
 /**
  * _strlen- print length of string
  * @s: string to count
- * @n: string
+ * 
  * Return: string length
  */
 int _strlen(char *s)
 {
 	int n;
-
-	n = 0;
 
 	for (; *s != '\0'; s++)
 	{
