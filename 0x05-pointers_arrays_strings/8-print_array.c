6adbdf7ef2fe1622aabec486e248212fb3618cc2
winifredaj <winifredajiniran@gmail.com>
2023-08-23 14:56:20 -0700

    Arrays are not pointers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,14 +11,13 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for ( i = 0; i <= n ; i++)
+	for (i = 0; i < n ; i++)
 	{
 		printf("%d", a[i]);
-		if ( i != (n - 1))
+		if (i != (n - 1))
 		{
 			printf(", ");
 		}
-	
 	}
 	_putchar('\n');
 }
