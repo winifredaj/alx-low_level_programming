aef8de68e849d72987790f2dae46cb14ab638497
winifredaj <winifredajiniran@gmail.com>
2023-08-23 12:57:31 -0700

    Half the lies they tell about me are not true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts- prints string
+ * @str: string to print
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i;
+	int j = 0;
+
+	while ( str[j] != '\0')
+	{
+		j++;
+	}
+	for (i = 0; i < j; i += 2)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
