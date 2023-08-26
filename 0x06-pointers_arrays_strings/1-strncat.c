fc7dd8b28e01214e65fee6ae9bb3cf1b50dca199
winifredaj <winifredajiniran@gmail.com>
2023-08-26 11:49:24 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,26 +6,23 @@
  * @dest: the destination string
  * @src: the source string
  * @n: limit of concantenation
- *
  * Return: nothing
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int dlen = 0, j = 0;
+	int dlen = 0;
+	int j = 0;
 
 	while (dest[dlen])
 	{
 		dlen++;
 	}
-
 	while (j < n && src[j])
 	{
 		dest[dlen] = src[j];
 		dlen++;
 		j++;
 	}
-
 	dest[dlen + n + 1] = '\0';
-
 	return (dest);
 }
