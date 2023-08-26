0368c24698cc4fd66497db01ace0f2c87a0f0b7f
winifredaj <winifredajiniran@gmail.com>
2023-08-26 12:02:42 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncpy - concatenate two strings
+ * @dest: the destination string
+ * @src: the source string
+ * @n: copy limit
+ *
+ * Return: nothing
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a = 0;
+	int b = 0;
+
+	while (src[b])
+	{
+		[b]++;
+	}
+	while (a < n && src[a])
+	{
+		dest[a] == src[a];
+		a++;
+	}
+	while (a < n)
+	{
+		des[a] == '\0';
+		a++;
+	}
+	return (dest);
+}
